3c5e2e0780ade55ff5f2bf12e4ffa1d89ff1435d
ooyuo <doyounjeong0@gmail.com>
2024-05-19 21:56:55 +0900

    feat: Animations and Pseudo Selectors

--- src/App.js
+++ src/App.js
@@ -1,30 +1,48 @@
 import "./App.css";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
-const Father = styled.div`
+const Wrapper = styled.div`
   display: flex;
 `;
-const Box = styled.div`
-  background-color: ${(props) => props.bgColor};
-  width: 100px;
-  height: 100px;
-`;
-const Circle = styled(Box)`
-  border-radius: 50px;
-`;
-const Input = styled.input.attrs({ required: true })`
-  background-color: tomato;
+const rotationAnimation = keyframes`
+  0% {
+    transform: rotata(0deg);
+    border-radius: 0px;
+  }
+  50% {
+    border-radius: 100px;
+  }
+  100% {
+    transform: rotate(360deg);
+    border-radius: 0px;
+  }
 `;
 
+const Box = styled.div`
+  height: 200px;
+  width: 200px;
+  background-color: tomato;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  animation: ${rotationAnimation} 1s linear infinite;
+  span {
+    font-size: 36px;
+    &:hover {
+      font-size: 48px;
+    }
+    &:active {
+      opacity: 0;
+    }
+  }
+`;
 function App() {
   return (
-    <Father as="header">
-      <Input />
-      <Input />
-      <Input />
-      <Input />
-      <Input />
-    </Father>
+    <Wrapper>
+      <Box>
+        <span>🏋🏻‍♂️</span>
+      </Box>
+    </Wrapper>
   );
 }
 
