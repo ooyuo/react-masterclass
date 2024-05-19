51310533721e99618c28851d3cdfe9b09ba55f88
ooyuo <doyounjeong0@gmail.com>
2024-05-19 21:32:31 +0900

    feat: styled-components

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import "./App.css";
+import styled from "styled-components";
+
+const Father = styled.div`
+  display: flex;
+`;
+const BoxOne = styled.div`
+  background-color: teal;
+  width: 100px;
+  height: 100px;
+`;
+const BoxTwo = styled.div`
+  background-color: tomato;
+  width: 100px;
+  height: 100px;
+`;
+const Text = styled.span`
+  color: 100px;
+`;
+
+function App() {
+  return (
+    <Father>
+      <BoxOne>
+        <Text></Text>
+      </BoxOne>
+      <BoxTwo />
+    </Father>
+  );
+}
+
+export default App;
