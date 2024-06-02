68faa4ce13d24c85236e22a66ba010b062c2982c
ooyuo <doyounjeong0@gmail.com>
2024-06-02 22:59:53 +0900

    feat: useParams

--- src/Router.tsx
+++ src/Router.tsx
@@ -5,6 +5,7 @@ import About from "./screens/About";
 import Root from "./Root";
 import ErrorComponent from "./componenets/ErrorComponent";
 import NotFound from "./screens/NotFound";
+import User from "./screens/Users/User";
 
 const router = createBrowserRouter([
   {
@@ -17,6 +18,10 @@ const router = createBrowserRouter([
         errorElement: <ErrorComponent />,
       },
       { path: "about", element: <About /> },
+      {
+        path: "users/:userId",
+        element: <User />,
+      },
     ],
     errorElement: <NotFound />,
   },
