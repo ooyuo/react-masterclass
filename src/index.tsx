907f55c2409ac37a1c8565254506f4b239cc62eb
ooyuo <doyounjeong0@gmail.com>
2024-06-02 22:30:15 +0900

    feat: create browser router

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
 import { darkTheme, lightTheme } from "./theme";
 import { ThemeProvider } from "styled-components";
-import { BrowserRouter, Router } from "react-router-dom";
+import { BrowserRouter, RouterProvider } from "react-router-dom";
+import router from "./Router";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <ThemeProvider theme={darkTheme}>
-        <App />
-      </ThemeProvider>
-    </BrowserRouter>
+    <ThemeProvider theme={darkTheme}>
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
